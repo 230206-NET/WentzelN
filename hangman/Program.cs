2c13f1cb594855600b8944310c0b1126a6f2f64a
Nick Wentzel <nckwntzl@gmail.com>
2023-02-09 16:34:23 -0500

    initial hangman

---
+++ hangman/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string temp;
+        hangman h = new hangman();
+        while(h.getNumGuess()<=6){
+            h.displayCurrentGuesses();
+            Console.WriteLine("Make a guess");
+            temp = Console.ReadLine();
+            if(temp.Length!=1)
+                Console.WriteLine("Please enter a single character");
+            else
+                h.makeGuess(temp[0]);
+        }
+    }
+}
+
+public class hangman{
+    private List<string> wordbank = new List<string>{ "default" };
+    private string chosenWord;
+    private List<char> guesses = new List<char>();
+    private int numGuess = 0;
+
+    public hangman()
+    {
+        Random rand = new Random();
+        this.chosenWord = wordbank[rand.Next(wordbank.Count())];
+    }
+
+    public void makeGuess(char s){
+        if(guesses.Contains(s)) return;
+        guesses.Add(s);
+        if(chosenWord.Contains(s)) Console.WriteLine("Correct!");
+        else
+        {
+            numGuess++;
+            Console.WriteLine("Incorrect");
+        }
+
+    }
+
+    public void displayCurrentGuesses(){
+        foreach (var c in chosenWord)
+            Console.Write(guesses.Contains(c) ? c : '_');
+        Console.Write('\n');
+
+    }
+
+    public int getNumGuess(){
+        return numGuess;
+    }
+
+}
