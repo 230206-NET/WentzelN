3815d0b9e92ad89e1a90fd1736c8737adc8207c1
Nick Wentzel <nckwntzl@gmail.com>
2023-02-10 15:07:56 -0500

    added test project

---
+++ test/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num = 1;
+while(true){
+    Console.Clear();
+    Console.Write(num);
+    num = (num % 10 == 0) ? 1 : num + 1;
+    System.Threading.Thread.Sleep(100);
+}
